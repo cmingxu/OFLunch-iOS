396e2e6bd2895ac3a5cd8802c4470690c68b0f34
kevin.xu <kevin.xu@aurorafeint.com>
2012-03-12 16:15:58 +0800

    Initial Commit

---
+++ OFLunchTests/OFLunchTests.h
@@ -0,0 +1,13 @@
+//
+//  OFLunchTests.h
+//  OFLunchTests
+//
+//  Created by kevin  on 3/12/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface OFLunchTests : SenTestCase
+
+@end
---
+++ OFLunchTests/OFLunchTests.m
@@ -0,0 +1,32 @@
+//
+//  OFLunchTests.m
+//  OFLunchTests
+//
+//  Created by kevin  on 3/12/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "OFLunchTests.h"
+
+@implementation OFLunchTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in OFLunchTests");
+}
+
+@end
